f5647c37c62c43b104cf012e2d8794be3542af91
IceRise <olegsever92@gmail.com>
2014-02-14 15:08:56 +0200

    Download

---
+++ State machine/State machine/Legs/LegTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_machine
+{
+    abstract class LegTypes
+    {
+        public double MaxSpeed { get; set; }
+        public double EnergyRequirements { get; set; }
+    }
+}
