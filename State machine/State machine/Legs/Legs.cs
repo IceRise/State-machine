e62a72e0eb36cc441b5f962c8ac022aaecb65c8d
IceRise <olegsever92@gmail.com>
2014-02-17 19:52:49 +0200

    Commit

---
+++ State machine/State machine/Legs/Legs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_machine
+{
+    abstract class Legs
+    {
+        protected double maxSpeed;
+        protected double Weight;
+        protected double CarryWeight;
+        protected double EnergyRequirements;
+
+        protected double currentSpeed;
+
+        public void SpeedPlus(double value)
+        {
+            currentSpeed += value;
+        }
+
+        public void SpeedMinus(double value)
+        {
+            if (currentSpeed > value)
+                currentSpeed -= value;
+            else
+                currentSpeed = 0;
+        }
+    }
+}
